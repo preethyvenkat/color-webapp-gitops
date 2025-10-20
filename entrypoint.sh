167108f1fae2b674b0be26c16bf3f81790bdae10
preethyvenkat <preevenkat@gmail.com>
2025-10-20 10:49:19 -0700

    test argocd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
+
 #!/bin/sh
 
 # Default color if none provided
-COLOR=${COLOR:-"#FF69B4"}
+COLOR=${COLOR:-"#4169E1"}
 
 # Inject the environment variable COLOR into the HTML template
 envsubst '$COLOR' < /usr/share/nginx/html/index.template.html > /usr/share/nginx/html/index.html
